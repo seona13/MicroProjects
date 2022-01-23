8f1e9b190b4245af0333dbdaeb8d6b9c9aa09ae7
Seona Bellamy <seona13@gmail.com>
2022-01-24 08:57:36 +1100

    Balloon Popper - Lesson 4
    - Added UI text to display score.
    - Added a UIManager to the canvas.

---
+++ Assets/BalloonPopper/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text _score;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
